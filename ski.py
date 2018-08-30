e211f3da53243d8997bea9ef5e2d423a2209e356
zhongh3 <zhon0017@gmail.com>
2018-08-31 00:31:27 +0800

    optimize the Area class and change ski_map to list of list so that the indexes denote the position

--- ski.py
+++ ski.py
@@ -2,17 +2,16 @@ import sys
 
 class Area:
     # an Area is a point on the Map
-    def __init__(self, x=-1, y=-1, height=-1, bottom_height=-1):
+    def __init__(self, x=-1, y=-1, height=-1):
         # the location coordinates (x, y) corresponding to the
         # row (x) and column (y) numbers on the Map (both 0-indexed)
+        # TODO: the position can be tracked by the indexes of the Area in ski_map
+        # TODO: decide whether to remove x & y later
         self.x = x
         self.y = y
 
         # height: corresponding to the value read from the Map
         self.height = height
-
-        # b_visited: a boolean, keeping track of whether the Area has been visited
-        self.b_visited = False
 
         # path_length: the longest length starting from the current Area
         # the default value '1' refers to the Area itself,
@@ -21,11 +20,12 @@ class Area:
 
         # bottom_height: the height of the lowest Area that can be visited from the current Area
         # i.e. the end of the path starting from current Area
-        self.bottom_height = bottom_height
+        # the default value '-1' indicates that the Area hasn't been visited
+        self.bottom_height = -1
 
     def __str__(self):
-        return "Area ({}, {}) - Height={}, Visited?={}, Path Length={}, Bottom Height={}"\
-            .format(self.x, self.y, self.height, self.b_visited, self.path_length, self.bottom_height)
+        return "Area ({}, {}) - Height={}, Path Length={}, Bottom Height={}" \
+            .format(self.x, self.y, self.height, self.path_length, self.bottom_height)
 
 
 def prepare_map(file_name):
@@ -46,17 +46,21 @@ def prepare_map(file_name):
         if len(input_row) != column:
             raise Exception("Line {} of input file has incorrect no. of values.".format(i + 2))
 
+        area_row = []
         for j in range(column):
-            ski_map.append(Area(i, j, input_row[j]))
+            area_row.append(Area(i, j, input_row[j]))
 
-    return ski_map
+        ski_map.append(area_row)
+
+    return ski_map, row, column
 
 
 def main():
-    ski_map = prepare_map("input.txt")
+    ski_map, row, column = prepare_map("input.txt")
 
-    for i in range(10):
-        print(ski_map[i])
+    for i in range(row):
+        for j in range(column):
+            print(ski_map[i][j])
 
 
 if __name__ == "__main__":
