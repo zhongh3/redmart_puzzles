c65c9e843958bcf9faa136afbf595c9e542ae0c6
zhongh3 <zhon0017@gmail.com>
2018-09-02 19:54:43 +0800

    optimaze code structure

--- ski.py
+++ ski.py
@@ -1,14 +1,12 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(funcName)s - %(lineno)d - %(message)s')
-
+# change logging level from INFO to DEBUG to print debugging logs
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(funcName)s - %(lineno)d - %(message)s')
 
 class Area:
     # an Area is a point on the Map
     def __init__(self, x=-1, y=-1, height=-1):
         # the location coordinates (x, y) corresponding to the
         # row (x) and column (y) numbers on the Map (both 0-indexed)
-        # TODO: the position can be tracked by the indexes of the Area in ski_map
-        # TODO: keep x & y for easier debugging first, can be removed later
         self.x = x
         self.y = y
 
@@ -33,16 +31,27 @@ class Area:
         return "Area ({}, {}) - Height={}, Visited?={}, Path Length={}, Bottom Height={}"\
             .format(self.x, self.y, self.height, self.b_visited, self.path_length, self.bottom_height)
 
+    def update_parameters(self, new_length, new_bottom):
+        # update both path length and bottom height if the new path length is longer
+        # when the old and new path lengths are equal, break tie with smaller bottom height
+        if self.path_length < new_length:
+            self.path_length = new_length
+            self.bottom_height = new_bottom
+        elif self.path_length == new_length:
+            self.bottom_height = min(new_bottom, self.bottom_height)
+
 
 def prepare_map(file_name):
     # parse input file to get the height of each Area
+    logging.info("Input File: \"./{}\"".format(file_name))
+
     # open the input file for reading
     file = open(file_name, "r")
 
     # the 1st line of the input file indicates the size of the map (row and column)
     row, column = map(int, file.readline().strip().split(" "))
 
-    logging.debug("row = {}, column = {}".format(row, column))
+    logging.info("Map Size: row = {}, column = {}".format(row, column))
 
     # ski_map is a list of lists where each entry is an Area object
     # the indexes of the entry denotes the location on the map
@@ -70,88 +79,82 @@ def prepare_map(file_name):
 
 
 def main():
-    def visit_area(area, x, y):
+    def visit_area(area):
 
-        def update_parameters(new_length, new_bottom):
-            # an helper function to update path_length and bottom_height
-            if area.path_length < new_length:
-                area.path_length = new_length
-                area.bottom_height = new_bottom
-            elif area.path_length == new_length:
-                area.bottom_height = min(new_bottom, area.bottom_height)
+        def compare_with_neighbour(area, neighbour):
+            # update path_length and bottom_height of current area after comparing with neighbour area
+            if area.height <= neighbour.height:
+                # the base case:
+                # the new path length is '1' (the current area itself)
+                # the new bottom height is the height of the current area itself
+                area.update_parameters(1, area.height)
+            else:
+                visit_area(neighbour)   # recursive call
+                new_length = neighbour.path_length + 1
+                new_bottom = neighbour.bottom_height
+                area.update_parameters(new_length, new_bottom)
 
         # if an Area has already been visited, the parameters are up-to-date
         if area.b_visited:
             return
 
-        # start to visit neighbours in clockwise order
+        # start to visit neighbours in clockwise order: N -> E -> S -> W
         # 1. North
-        if x == 0 or area.height <= ski_map[x - 1][y].height:
-            area.path_length = 1
-            area.bottom_height = area.height
+        if area.x == 0:  # there's no neighbour to the North of current area -> the base case
+            area.update_parameters(1, area.height)
         else:
-            visit_area(ski_map[x - 1][y], x - 1, y)
-            area.path_length = ski_map[x - 1][y].path_length + 1
-            area.bottom_height = ski_map[x - 1][y].bottom_height
+            neighbour = ski_map[area.x - 1][area.y]
+            compare_with_neighbour(area, neighbour)
 
         # 2. East
-        if y == (column - 1) or area.height <= ski_map[x][y + 1].height:
-            east_length = 1
-            east_bottom = area.height
+        if area.y == (column - 1):  # there's no neighbour to the East of current area -> the base case
+            area.update_parameters(1, area.height)
         else:
-            visit_area(ski_map[x][y + 1], x, y + 1)
-            east_length = ski_map[x][y + 1].path_length + 1
-            east_bottom = ski_map[x][y + 1].bottom_height
-
-        update_parameters(east_length, east_bottom)
+            neighbour = ski_map[area.x][area.y + 1]
+            compare_with_neighbour(area, neighbour)
 
         # 3. South
-        if x == (row - 1) or area.height <= ski_map[x + 1][y].height:
-            south_length = 1
-            south_bottom = area.height
+        if area.x == (row - 1):  # there's no neighbour to the South of current area -> the base case
+            area.update_parameters(1, area.height)
         else:
-            visit_area(ski_map[x + 1][y], x + 1, y)
-            south_length = ski_map[x + 1][y].path_length + 1
-            south_bottom = ski_map[x + 1][y].bottom_height
-
-        update_parameters(south_length, south_bottom)
+            neighbour = ski_map[area.x + 1][area.y]
+            compare_with_neighbour(area, neighbour)
 
         # 4. West
-        if y == 0 or area.height <= ski_map[x][y - 1].height:
-            west_length = 1
-            west_bottom = area.height
+        if area.y == 0:  # there's no neighbour to the West of current area -> the base case
+            area.update_parameters(1, area.height)
         else:
-            visit_area(ski_map[x][y - 1], x, y - 1)
-            west_length = ski_map[x][y - 1].path_length + 1
-            west_bottom = ski_map[x][y - 1].bottom_height
+            neighbour = ski_map[area.x][area.y - 1]
+            compare_with_neighbour(area, neighbour)
 
-        update_parameters(west_length, west_bottom)
-
+        # all neighbours have been visited, the current area has been updated/visited as well
         area.b_visited = True
 
-        return
+        return area.height - area.bottom_height
 
     # ski_map is a list of lists with size row x column, where each entry is an Area
+    # the indexes of each Area in ski_map is their location on the map
     ski_map, row, column = prepare_map("input.txt")
 
+    # the minimum of max_length should be '1' and max_drop should be '0'
+    # init both to negative values and update them accordingly
     max_length = -1
     max_drop = -1
 
     for i in range(row):
         for j in range(column):
             area = ski_map[i][j]
-            visit_area(area, i, j)                    
+            drop = visit_area(area)
             if max_length < area.path_length:
                 logging.debug(area)
                 max_length = area.path_length
-                max_drop = area.height - area.bottom_height
+                max_drop = drop
             elif max_length == area.path_length:
+                # break tie with larger drop
                 logging.debug(area)
-                max_drop = max(area.height - area.bottom_height, max_drop)
+                max_drop = max(drop, max_drop)
 
-    print("max length = {}, max drop = {}".format(max_length, max_drop))
-
-    return
+    print("Results: max length = {}, max drop = {}".format(max_length, max_drop))
 
 
 if __name__ == "__main__":
